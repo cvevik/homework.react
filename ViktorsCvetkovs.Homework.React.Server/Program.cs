45f5fac66d778ce9df84fc1be92496f7325434a7
Viktors Cvetkovs <vcvetkovs@outlook.com>
2023-11-20 08:21:30 +0200

    Remove Azure KeyVault.

--- ViktorsCvetkovs.Homework.React.Server/Program.cs
+++ ViktorsCvetkovs.Homework.React.Server/Program.cs
@@ -2,18 +2,17 @@ using Azure.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var keyVaultUrl = builder.Configuration["KeyVaultConfiguration:KeyVaultURL"]!;
-var keyVaultClientId = builder.Configuration["KeyVaultConfiguration:ClientId"]!;
-var keyVaultClientSecret = builder.Configuration["KeyVaultConfiguration:ClientSecret"]!;
+// TODO: Uncomment for KeyVault
+//var keyVaultUrl = builder.Configuration["KeyVaultConfiguration:KeyVaultURL"]!;
+//var keyVaultClientId = builder.Configuration["KeyVaultConfiguration:ClientId"]!;
+//var keyVaultClientSecret = builder.Configuration["KeyVaultConfiguration:ClientSecret"]!;
 
-builder.Configuration.AddAzureKeyVault(
-        new Uri(keyVaultUrl),
-        new ClientSecretCredential(
-            "98f64ac9-ff7e-42f1-afef-06a12247cb4a",
-            keyVaultClientId,
-            keyVaultClientSecret ));
-        //keyVaultClientId,
-        //keyVaultClientSecret);
+//builder.Configuration.AddAzureKeyVault(
+//        new Uri(keyVaultUrl),
+//        new ClientSecretCredential(
+//            "98f64ac9-ff7e-42f1-afef-06a12247cb4a",
+//            keyVaultClientId,
+//            keyVaultClientSecret ));
 
 // Add services to the container.
 builder.Services.AddControllers();
